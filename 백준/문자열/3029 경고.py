8d2327fbca32112366714cfbe72c7a9d48b14ac8
stopkite <stopkite98@naver.com>
2023-03-28 17:38:04 +0900

    [백준] 3209 경고

---
+++ 백준/문자열/3029 경고.py
@@ -0,0 +1,16 @@
+h1, m1, s1 = map(int, input().split(':'))
+h2, m2, s2 = map(int, input().split(':'))
+
+t1 = (h1 * 60 * 60) + (m1 * 60) + s1
+t2 = (h2 * 60 * 60) + (m2 * 60) + s2
+
+if t1 > t2:
+    t = t2 - t1 + (24 * 60 * 60)
+else:
+    t = t2 - t1
+
+h = t // 60 // 60
+m = t // 60 % 60
+s = t % 60
+
+print(f"{h:02d}:{m:02d}:{s:02d}")
