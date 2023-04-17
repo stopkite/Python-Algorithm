0a170c6bdfd65942aa704438c3a722e37fc68096
stopkite <stopkite98@naver.com>
2023-04-17 14:55:14 +0900

    [코드트리] 문자열) 공백있는 문자열 한꺼번에 입력받기

---
+++ 코드트리/문자열/공백있는 문자열 한꺼번에 입력받기/공백을 기준으로 출력.py
@@ -0,0 +1,8 @@
+word1 = input().split()
+word2 = input().split()
+
+for elem in word1:
+    print(elem, end="")
+
+for elem in word2:
+    print(elem, end="")
---
+++ 코드트리/문자열/공백있는 문자열 한꺼번에 입력받기/문자 개수 세기.py
@@ -0,0 +1,4 @@
+word = input()
+w = input()
+
+print(word.count(w))
