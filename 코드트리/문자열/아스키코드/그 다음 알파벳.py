52c6fbb6963c6b29cfe23a29c61a25ba56da188e
stopkite <stopkite98@naver.com>
2023-04-24 21:16:18 +0900

    [코드트리] 문자열) 아스키코드

---
+++ 코드트리/문자열/아스키코드/그 다음 알파벳.py
@@ -0,0 +1,6 @@
+a = input()
+
+if a =='z':
+    print('a')
+else:
+    print(chr(ord(a)+1))
---
+++ 코드트리/문자열/아스키코드/아스키코드의 합과 차.py
@@ -0,0 +1,6 @@
+a,b = input().split()
+
+add_value = ord(a) + ord(b)
+diff_value = abs(ord(a) - ord(b))
+
+print(add_value, diff_value)
