87711fba82d7dc6582742a342774097ea9c8b32f
stopkite <stopkite98@naver.com>
2023-04-26 13:50:57 +0900

    [백준] 2422 한윤정이 이탈리아에서 아이스크림을 사먹는데

---
+++ 백준/완전탐색/2422 한윤정이 이탈리아에서 아이스크림을 사먹는데.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+n, m = map(int, input().split())
+ice = [[False] * n for _ in range(n)]
+for i in range(m):
+    a, b = map(int, input().split())
+    ice[a - 1][b - 1] = True
+    ice[b - 1][a - 1] = True
+
+result = 0
+
+for i in combinations(range(n), 3):
+    if ice[i[0]][i[1]] or ice[i[0]][i[2]] or ice[i[1]][i[2]]:
+        continue
+    result += 1
+
+print(result)
