f8cc9e0b43e71150451586d6c0cfa1adc7cd4329
stopkite <stopkite98@naver.com>
2023-04-29 14:05:39 +0900

    [백준] 17413 단어 뒤집기2

---
+++ 백준/문자열/17413 단어 뒤집기2.py
@@ -0,0 +1,27 @@
+s = input()
+
+temp = ''
+ans = ''
+
+for i in s:
+    if i == ' ':
+        if '<' not in temp:
+            ans += temp[::-1] + i
+            temp = ''
+        else:
+            temp += i
+
+    elif i == '<':
+        ans += temp[::-1]
+        temp = ''
+        temp += i
+
+    elif i == '>':
+        ans += temp + i
+        temp = ''
+    else:
+        temp += i
+
+ans += temp[::-1]
+
+print(ans)
