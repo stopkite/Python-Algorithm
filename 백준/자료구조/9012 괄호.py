d0645457f900ae2e4b596f3fb3b5f229e7cb54a2
stopkite <stopkite98@naver.com>
2023-05-01 18:56:45 +0900

    [백준] 9012 괄호

---
+++ 백준/자료구조/9012 괄호.py
@@ -0,0 +1,28 @@
+def solution(n: int):
+    for _ in range(n):
+        arr = list(input())
+        stack = []
+
+        flag = True
+        for elem in arr:
+            if elem == '(':
+                stack.append('(')
+            elif elem == ')':
+                if len(stack) != 0:
+                    stack.pop()
+                else:  # stack 안이 비어 있으면 False
+                    flag = False
+                    break
+            else:  # '(', ')'를 제외한 문자열이 들어오면 continue
+                continue
+
+        if len(stack) != 0:  # 스택 안에 값이 남아 있으면 False
+            flag = False
+
+        if flag:  # 괄호 문자열 여부 확인
+            print("YES")
+        else:
+            print("NO")
+
+
+solution(int(input()))
