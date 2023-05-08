03861e8c74dc4228e2a6906ef47741f122bcc336
stopkite <stopkite98@naver.com>
2023-05-08 16:27:30 +0900

    [백준] 1966 프린터 큐

--- 백준/자료구조/1966 프린터 큐.py
+++ 백준/자료구조/1966 프린터 큐.py
@@ -3,13 +3,20 @@ from collections import deque
 t = int(input())
 
 for _ in range(t):
-    n, k = map(int, input().split())
+    n, m = map(int, input().split())
     q = deque(list(map(int, input().split())))
-    order = deque(list(range(n)))
-
+    count = 0
     while q:
-        if q[0] == max(q):
-            q.popleft()
+        max_value = max(q)
+        front = q.popleft()
+        m -= 1
 
-
-
+        if max_value == front:
+            count += 1
+            if m < 0:
+                print(count)
+                break
+        else:
+            q.append(front)
+            if m < 0:
+                m = len(q) - 1
