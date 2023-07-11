31290ed6b7576b33a75d736b3a3699fd77a8cb54
stopkite <stopkite98@naver.com>
2023-07-11 16:49:34 +0900

    [백준]2606 바이러스

---
+++ 백준/bfs, dfs/2606 바이러스.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+
+def solution():
+    n = int(input())
+    m = int(input())
+    cnt = 0
+
+    graph = [[False] * (n + 1) for _ in range(n + 1)]
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[a][b] = True
+        graph[b][a] = True
+
+    visited = [False] * (n + 1)
+
+    visited[1] = True
+    queue = deque([1])
+    while queue:
+        v = queue.popleft()
+        cnt += 1
+        for i in range(1, n + 1):
+            if not visited[i] and graph[v][i]:
+                visited[i] = True
+                queue.append(i)
+
+    answer = cnt - 1
+    print(answer)
+
+
+solution()
