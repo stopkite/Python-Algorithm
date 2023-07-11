d23d911a995d061909bb9b3e26247b536b8d7259
stopkite <stopkite98@naver.com>
2023-07-11 15:14:20 +0900

    [백준](repeat) 1260 DFS와 BFS

--- 백준/bfs, dfs/1260 dfs와 bfs.py
+++ 백준/bfs, dfs/1260 dfs와 bfs.py
@@ -1,41 +1,43 @@
 from collections import deque
 
-n, m, v = map(int, input().split())
 
-graph = [[False] * (n + 1) for _ in range(n + 1)]
+def solution(n, m, v):
+    # 그래프 초기화
+    graph = [[False] * (n + 1) for _ in range(n + 1)]
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[a][b] = True
+        graph[b][a] = True
 
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = True
-    graph[b][a] = True
-
-
-def dfs(start):
-    visited[start] = True
-    print(start, end=" ")
-    for i in range(1, n + 1):
-        if not visited[i] and graph[start][i]:
-            dfs(i)
-
-
-def bfs(start):
-    visited[start] = True
-    queue = deque([start])
-    while queue:
-        V = queue.popleft()
-        print(V, end=" ")
+    # dfs
+    def dfs(start, visited):
+        visited[start] = True
+        print(start, end=" ")
         for i in range(1, n + 1):
-            if not visited[i] and graph[V][i]:
+            if not visited[i] and graph[start][i]:
                 visited[i] = True
-                queue.append(i)
+                dfs(i, visited)
 
-    return visited
+    # bfs
+    def bfs(start, visited):
+        visited[start] = True
+        queue = deque([start])
+        while queue:
+            i = queue.popleft()
+            print(i, end=" ")
+            for j in range(1, n + 1):
+                if not visited[j] and graph[i][j]:
+                    visited[j] = True
+                    queue.append(j)
+
+    visited = [False] * (n + 1)
+    dfs(v, visited)
+
+    print()
+
+    visited = [False] * (n + 1)
+    bfs(v, visited)
 
 
-visited = [False] * (n + 1)
-dfs(v)
-
-print()
-
-visited = [False] * (n + 1)
-bfs(v)
+n, m, v = map(int, input().split())
+solution(n, m, v)
