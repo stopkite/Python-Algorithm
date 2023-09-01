dbe43d979fc05c14cb9c2a255e1e20c8dba97cf3
stopkite <stopkite98@gmail.com>
2023-09-01 10:57:40 +0900

    [백준] 1744 수묶기

--- 백준/그리디/1744 수 묶기.py
+++ 백준/그리디/1744 수 묶기.py
@@ -3,38 +3,31 @@ numbers = [int(input()) for _ in range(n)]
 
 positive_nums = []
 negative_nums = []
-is_zero_exist = False
+
+result = 0
 for num in numbers:
-    if num == 0:
-        is_zero_exist = True
-    elif num > 0:
+    if num > 1:
         positive_nums.append(num)
-    else:
+    elif num <= 0:
         negative_nums.append(num)
-
-positive_nums.sort()  # 1 3 5
-negative_nums.sort()  # -5 -3 -1
-
-# 음수 o, 0 존재 -> 가장 큰 음수랑 곱하기
-# 음수 o, 0 부재 -> 음수까리 곱하기
-# 음수 x, 0 존재 -> 더하기
-# 음수 x, 0 부재 -> 양수 곱하기
-total_num = 0
-if negative_nums:
-    if is_zero_exist:
-        negative_nums.pop()  # 0 이랑 곱하기 때문에 없애주기
-        while len(negative_nums) != 1:  # 하나 남을 때까지 뽑기
-            n1 = negative_nums.pop()
-            n2 = negative_nums.pop()
-            total_num += (n1 * n2)
     else:
-        while len(negative_nums) != 1:
-            n1 = negative_nums.pop()
-            n2 = negative_nums.pop()
-            total_num += (n1 * n2)
-else:
-    if is_zero_exist:
-        while len(negative_nums)
+        result += num
 
+positive_nums.sort(reverse=True)
+negative_nums.sort()
 
+# 양수 묶기
+for i in range(0, len(positive_nums), 2):
+    if i + 1 >= len(positive_nums):
+        result += positive_nums[i]
+    else:
+        result += (positive_nums[i] * positive_nums[i + 1])
 
+# 음수 묶기
+for i in range(0, len(negative_nums), 2):
+    if i + 1 >= len(negative_nums):
+        result += negative_nums[i]
+    else:
+        result += (negative_nums[i] * negative_nums[i + 1])
+
+print(result)
