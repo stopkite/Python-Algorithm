68d8cb16c0ae3c2ae8d3cb44026e10ebf848939b
stopkite <stopkite98@gmail.com>
2024-01-10 01:48:03 +0900

    [LTC] Keys and Rooms_bfs / Medium / 15분

---
+++ 리트코드/Keys and Rooms_bfs.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+
+class Solution:
+    def canVisitAllRooms(self, rooms):
+        visited = [False] * len(rooms)
+
+        queue = deque([0])
+        while queue:
+            cur_v = queue.popleft()
+            visited[cur_v] = True
+            for v in rooms[cur_v]:
+                if not visited[v]:
+                    visited[v] = True
+                    queue.append(v)
+
+        return all(visited)
+
+solution = Solution()
+solution.canVisitAllRooms([[1],[2],[3],[]])
