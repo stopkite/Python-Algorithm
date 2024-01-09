483dc5cb86579a8968dd06ff35d46a6e639616a1
stopkite <stopkite98@gmail.com>
2024-01-09 19:40:52 +0900

    [LTC] Keys and Rooms_dfs / Medium / 40분

---
+++ 리트코드/Keys and Rooms_dfs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canVisitAllRooms(self, rooms):
+        visited = [False] * len(rooms)
+
+        def dfs(cur_v):
+            visited[cur_v] = True
+            for next_v in rooms[cur_v]:
+                if not visited[next_v]:
+                    dfs(next_v)
+
+        dfs(0)
+
+        return all(visited)
+
